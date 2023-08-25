ce4ac8d3e7c29f70689183e66b59678d3d677c2a
Mohit kachhaway <130671896+MohitKachhway@users.noreply.github.com>
2023-08-25 08:06:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+// //your JS code here. If required.
+// let form = document.querySelector("form");
+// form.addListener("submit",()=>{
+// 	event.preventDefault();
+// let text = document.getElementById("text");
+// let delay = document.getElementById("delay");
+// let output = document.getElementById("output");
+// setTimeout(()=>{
+// 	output.textContent=text.value;
+// },delay.value);
+	
+// })
+let form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // Prevent the default form submission behavior
+
+  let text = document.getElementById("text");
+  let delay = document.getElementById("delay");
+  let output = document.getElementById("output");
+
+  setTimeout(() => {
+    output.textContent = text.value;
+  }, parseInt(delay.value));
+});
+
+
